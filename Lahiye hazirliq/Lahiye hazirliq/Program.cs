f184ec4c6886ea391568b232a4faaabac238e40a
AsgarMadadli <asgarmm@code.edu.az>
2023-07-20 03:19:59 -0700

    Define the Teacher class

---
+++ Lahiye hazirliq/Lahiye hazirliq/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Teacher
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Subject { get; set; }
+    public List<Student> Students { get; set; }
+
+    public Teacher(string name, int age, string subject)
+    {
+        Name = name;
+        Age = age;
+        Subject = subject;
+        Students = new List<Student>();
+    }
+
+    public override string ToString()
+    {
+        return $"Teacher: {Name}, Age: {Age}, Subject: {Subject}";
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Grade { get; set; }
+
+    public Student(string name, int age, string grade)
+    {
+        Name = name;
+        Age = age;
+        Grade = grade;
+    }
+}
---
+++ Lahiye_hazirliq/Lahiye_hazirliq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lahiye_hazirliq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
